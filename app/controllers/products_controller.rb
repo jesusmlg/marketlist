0d10d063f0878cffd31d1a8d5b1c75d39c438831
Jesús María Luis Gil <jesusmlg@gmail.com>
2016-11-28 16:46:13 +0100

    h

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -61,14 +61,14 @@ class ProductsController < ApplicationController
 			params.require(:product).permit(:name,:user)
 		end
 
-		def allowed?
-			if session[:user].nil?
-				if params[:pass]!=ENV['OPENSHIFT_MARKETLIST_JMLGKEY']
-					redirect_to 'http://www.google.es'
-				else
-					session[:user] = params[:user]
-					redirect_to root_path
-				end
-			end
-		end
+		# def allowed?
+		# 	if session[:user].nil?
+		# 		if params[:pass]!=ENV['OPENSHIFT_MARKETLIST_JMLGKEY']
+		# 			redirect_to 'http://www.google.es'
+		# 		else
+		# 			session[:user] = params[:user]
+		# 			redirect_to root_path
+		# 		end
+		# 	end
+		# end
 end
